826903e982a101880053c6a087d4cc322b8f2d66
michaelskyba <michaelskyba1411@gmail.com>
2022-01-25 17:35:42 -0500

    math tool in TypeScript

--- tools/math.js
+++ tools/math.js
@@ -1,20 +1,16 @@
-let output = document.getElementById("math_output")
-let input = document.getElementById("math_input")
-
-document.getElementById("render").onclick = function() {
-	// Delete existing elements
-	while (output.children.length != 0) {
-		output.children[0].remove()
-	}
-
-	// Create an element for each line of math input
-	let inputs = input.value.split("\n")
-	inputs.forEach(function(value, index) {
-		let p = document.createElement("p")
-		p.innerHTML = "`" + value + "`"
-		output.appendChild(p)
-	})
-
-	// Tell MathJax to re-render
-	MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'math-display']);
-}
+var output = document.getElementById("math_output");
+var input = document.getElementById("math_input");
+document.getElementById("render").onclick = function () {
+    // Delete existing elements
+    while (output.children.length != 0) {
+        output.children[0].remove();
+    }
+    // Create an element for each line of math input
+    input.value.split("\n").forEach(function (line) {
+        var p = document.createElement("p");
+        p.innerHTML = "`" + line + "`";
+        output.appendChild(p);
+    });
+    // Tell MathJax to re-render
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'math-display']);
+};
---
+++ tools/math.ts
@@ -0,0 +1,22 @@
+declare var MathJax
+
+let output = document.getElementById("math_output")
+let input = <HTMLInputElement>document.getElementById("math_input")
+
+document.getElementById("render").onclick = () => {
+	// Delete existing elements
+	while (output.children.length != 0) {
+		output.children[0].remove()
+	}
+
+	// Create an element for each line of math input
+	input.value.split("\n").forEach(line => {
+		let p = document.createElement("p")
+		p.innerHTML = "`" + line + "`"
+
+		output.appendChild(p)
+	})
+
+	// Tell MathJax to re-render
+	MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'math-display']);
+}
