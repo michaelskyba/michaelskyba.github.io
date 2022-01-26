2e6617948d678955972e6270e56eb71678709783
michaelskyba <michaelskyba1411@gmail.com>
2022-01-25 19:38:27 -0500

    time tool in TypeScript

--- tools/time.js
+++ tools/time.js
@@ -1,34 +1,35 @@
+var invalid = "Invalid Date";
 // Is this syntax idiomatic? It looks weird
-const get_unix = (date) => Math.floor(date.getTime() / 1000)
-
-let time_input  = document.getElementById("time_input")
-document.getElementById("current").innerHTML = get_unix(new Date())
-
-document.getElementById("convert").onclick = function() {
-	let output = []
-
-	let conversions = time_input.value.split("\n")
-	conversions.forEach((value, index) => {
-
-		// Skip blank lines
-		if (value == "") output.push("")
-
-		// Don't append a bunch of them if the user is spamming the button
-		else if (value.includes("Invalid Date")) output.push(value)
-
-		// User provided Unix time
-		else if (!isNaN(value)) output.push(new Date(parseInt(value) * 1000))
-
-		// User supposedly gave a date
-		else {
-			let date = new Date(value)
-
-			let invalid = "Invalid Date"
-			if (date.toString() == invalid) output.push(invalid + ": " + value)
-
-			else output.push(get_unix(date))
-		}
-	})
-
-	time_input.value = output.join("\n")
-}
+var get_unix = function (date) { return Math.floor(date.getTime() / 1000).toString(); };
+/*
+"/>" fixes Kakoune's syntax highlighting
+It's ugly but there's no way I'll be able to fix the javascript.kak regex myself
+I'd guess it has to do with the jsx sections
+I'm too scared that it's my fault to ask about it on the IRC
+*/
+var time_input = document.getElementById("time_input"); // />
+document.getElementById("current").innerHTML = get_unix(new Date());
+document.getElementById("convert").onclick = function () {
+    var output = [];
+    var conversions = time_input.value.split("\n");
+    conversions.forEach(function (value) {
+        // Skip blank lines
+        if (value == "")
+            output.push("");
+        // Don't append a bunch of them if the user is spamming the button
+        else if (value.includes(invalid))
+            output.push(value);
+        // User provided Unix time
+        else if (!isNaN(+value))
+            output.push(new Date(parseInt(value) * 1000).toString());
+        // User supposedly gave a date
+        else {
+            var date = new Date(value);
+            if (date.toString() == invalid)
+                output.push(invalid + ": " + value);
+            else
+                output.push(get_unix(date));
+        }
+    });
+    time_input.value = output.join("\n");
+};
---
+++ tools/time.ts
@@ -0,0 +1,41 @@
+const invalid = "Invalid Date"
+
+// Is this syntax idiomatic? It looks weird
+const get_unix = (date) => Math.floor(date.getTime() / 1000).toString()
+
+/*
+"/>" fixes Kakoune's syntax highlighting
+It's ugly but there's no way I'll be able to fix the javascript.kak regex myself
+I'd guess it has to do with the jsx sections
+I'm too scared that it's my fault to ask about it on the IRC
+*/
+
+let time_input = <HTMLInputElement>document.getElementById("time_input") // />
+document.getElementById("current").innerHTML = get_unix(new Date())
+
+document.getElementById("convert").onclick = function() {
+	let output: string[] = []
+
+	let conversions = time_input.value.split("\n")
+	conversions.forEach(value => {
+
+		// Skip blank lines
+		if (value == "") output.push("")
+
+		// Don't append a bunch of them if the user is spamming the button
+		else if (value.includes(invalid)) output.push(value)
+
+		// User provided Unix time
+		else if (!isNaN(+value)) output.push(new Date(parseInt(value) * 1000).toString())
+
+		// User supposedly gave a date
+		else {
+			let date = new Date(value)
+
+			if (date.toString() == invalid) output.push(invalid + ": " + value)
+			else output.push(get_unix(date))
+		}
+	})
+
+	time_input.value = output.join("\n")
+}
