f683586b4cf895f0b53fe7ff705dd209a59db840
michaelskyba <michaelskyba1411@gmail.com>
2022-04-22 13:33:19 -0400

    games/2022/magic-ball

---
+++ games/2022/magic-ball/index.ts
@@ -0,0 +1,17 @@
+function getResponse(): string {
+	return [
+		"Outlook: not so good.",
+		"Don't count on it.",
+		"My sources say no.",
+		"Without a doubt.",
+		"Reply hazy. Try again.",
+		"It is certain.",
+		"My reply is 'no'.",
+		"As I see it, yes."
+	]
+	[Math.floor(Math.random() * 8)]
+}
+
+document.getElementById("submit").onclick = () => {
+	document.getElementById("output").innerHTML = getResponse()
+}
