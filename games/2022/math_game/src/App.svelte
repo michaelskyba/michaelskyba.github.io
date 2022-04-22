af8191ed5eb5d53ab0f0dc8299173ccab7ea15bc
michaelskyba <michaelskyba1411@gmail.com>
2022-04-22 13:39:48 -0400

    games/2022: underscores instead of dashes

---
+++ games/2022/math_game/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import MainMenu from "./MainMenu.svelte"
+
+	let screen = "Main menu"
+</script>
+
+<h1>Arithmetic Game: {screen}</h1>
+<p>Don't use a calculator.</p>
+<hr>
+
+{#if screen == "Main menu"}
+	<MainMenu />
+{:else}
+	<p>Not Main menu</p>
+{/if}
+
+<hr>
+<p>by Michael Skyba</p>
---
+++ games/2022/math_game/src/MainMenu.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<input type="button" value="Shop">
