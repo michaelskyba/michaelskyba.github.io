9fab9664d7021309dd043d22cb7c978c3ae71a94
michaelskyba <michaelskyba1411@gmail.com>
2022-04-27 13:04:26 -0400

    MainMenu.svelte: implement main menu

--- games/2022/math_game/src/App.svelte
+++ games/2022/math_game/src/App.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
 	import MainMenu from "./MainMenu.svelte"
 
+	let unlockedScreens = {
+		"Addition": true,
+		"Subtraction": false,
+		"Multiplication": false,
+		"Division": true,
+		"Exponents": false,
+		"Roots": false,
+		"Factoring": true,
+		"Shop": true
+	}
+
+	let unlockedFeatures = {
+		"difficulty": false,
+		"redirection": false
+	}
+
 	let screen = "Main menu"
 </script>
 
@@ -9,7 +25,7 @@
 <hr>
 
 {#if screen == "Main menu"}
-	<MainMenu />
+	<MainMenu {unlockedScreens} bind:screen={screen} />
 {:else}
 	<p>Not Main menu</p>
 {/if}
--- games/2022/math_game/src/MainMenu.svelte
+++ games/2022/math_game/src/MainMenu.svelte
@@ -1,4 +1,33 @@
 <script lang="ts">
+	export let unlockedScreens: object
+	export let screen: string
+
+	let screenNames = [
+		"Addition",
+		"Subtraction",
+		"Multiplication",
+		"Division",
+		"Exponents",
+		"Roots",
+		"Factoring",
+		"Shop"
+	]
 </script>
 
-<input type="button" value="Shop">
+{#each screenNames as screenName}
+	{#if unlockedScreens[screenName]}
+		<input
+			type="button"
+			value={screenName}
+			on:click={() => screen = screenName}
+		>
+	{/if}
+{/each}
+
+<style>
+	input[type="button"] {
+		width: 100%;
+		height: 50px;
+		font-size: 20px;
+	}
+</style>
