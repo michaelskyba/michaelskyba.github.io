7980a326c99a7bb71f22404ef65eeebd1d97c097
michaelskyba <michaelskyba1411@gmail.com>
2022-04-28 13:52:20 -0400

    Math.svelte: basic input system

--- games/2022/math_game/src/Math.svelte
+++ games/2022/math_game/src/Math.svelte
@@ -2,5 +2,25 @@
 	export let questions
 	export let screen
 
-	console.log(questions[screen](5, 10))
+	let difficulty = 1
+	let input = ""
+
+	function questionValues(diff: int) {
+		return [5, 10]
+	}
+
+	let values = questionValues(difficulty)
+	let pair = questions[screen](values[0], values[1])
+
+	const submit = () => {
+		if (parseInt(input) == pair.answer) console.log("correct")
+	}
+
+	document.onkeydown = e => {
+		if (e.keyCode == 13) submit()
+	}
 </script>
+
+<h3>{pair.question}</h3>
+<input type="number" placeholder="Answer" bind:value={input}>
+<input type="button" value="Submit" on:click={submit}>
