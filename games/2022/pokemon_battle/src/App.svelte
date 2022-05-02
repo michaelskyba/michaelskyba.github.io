33773efaf17bd91d31037a831c9707d980aef358
michaelskyba <michaelskyba1411@gmail.com>
2022-05-02 10:36:56 -0400

    App.svelte: basic gameplay loop

--- games/2022/pokemon_battle/src/App.svelte
+++ games/2022/pokemon_battle/src/App.svelte
@@ -18,6 +18,59 @@
 		"hp": 3,
 		"power": 1000
 	}
+
+	const RNG = (min, max) => {
+		return Math.round(Math.random() * (max - min)) + min
+	}
+
+	let expended
+	const expend = () => {
+		let p = parseInt(expended)
+		if (Number.isNaN(p) || p > player.power || p < 0) {
+			// We need log = <> for it to count as updated
+			log = [`Invalid power provided: ${expended}`, ...log]
+			return
+		}
+
+		// We could cheat and have p2 = p + 1 but that would be unfair.
+
+		let p2 = RNG(0, computer.power)
+
+		player.power -= p
+		computer.power -= p2
+
+		let outcome = []
+		if (p > p2) {
+			outcome = [
+				"Computer loses 1 HP",
+				"Player gains 1 HP",
+				"Player wins exchange"
+			]
+
+			computer.hp--
+			player.hp++
+		}
+
+		else if (p2 > p) {
+			outcome = [
+				"Player loses 1 HP",
+				"Computer gains 1 HP",
+				"Computer wins exchange"
+			]
+
+			player.hp--
+			computer.hp++
+		}
+
+		else outcome = ["Tied exchange"]
+
+		log = [
+			...outcome,
+			`Player bets ${p}`,
+			`Computer bets ${p2}`,
+			...log
+		]
+	}
 </script>
 
 <h1>Turn Battle</h1>
@@ -27,8 +80,8 @@
 <hr>
 <Player player={player} />
 
-<input type="number" placeholder="Power">
-<input type="button" value="Expend">
+<input type="number" placeholder="Power" bind:value={expended}>
+<input type="button" value="Expend" on:click={expend}>
 
 <Log {log} />
 
