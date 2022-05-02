b6efb5f4dea0aa6c6a1b7c27404f072a87f30d1f
michaelskyba <michaelskyba1411@gmail.com>
2022-05-02 13:45:54 -0400

    implement taking / eliminating

--- games/2022/21/src/Game.svelte
+++ games/2022/21/src/Game.svelte
@@ -1,9 +1,31 @@
 <script>
 	export let players
 	let eliminated = []
+	let dead = false
 
 	let turn = 0
 	let counter = 21
+
+	const take = num => {
+		counter -= num
+
+		if (counter < 1) {
+			counter = 21
+
+			// Player lost
+			eliminated = [...eliminated, players[turn]]
+
+			// players.splice(turn, 1) doesn't update.
+			let second = []
+			if (turn < players.length - 1) second = [...players.slice(turn+1, players.length)]
+			players = [...players.slice(0, turn), ...second]
+
+			dead = true
+		}
+
+		turn++
+		if (turn >= players.length) turn = 0
+	}
 </script>
 
 <label for="players">In-game</label>
@@ -13,7 +35,7 @@
 	{/each}
 </ol>
 
-{#if eliminated.length > 0}
+{#if dead}
 	<label for="eliminated">Eliminated</label>
 	<ol id="eliminated">
 		{#each eliminated as player}
@@ -27,9 +49,9 @@
 <hr>
 
 <h3>Counter: {counter}</h3>
-<input type="button" value="Take 1" on:click={take(1)} />
-<input type="button" value="Take 2" on:click={take(2)} />
-<input type="button" value="Take 3" on:click={take(3)} />
+<input type="button" value="Take 1" on:click={() => take(1)} />
+<input type="button" value="Take 2" on:click={() => take(2)} />
+<input type="button" value="Take 3" on:click={() => take(3)} />
 
 <style>
 	ol {
