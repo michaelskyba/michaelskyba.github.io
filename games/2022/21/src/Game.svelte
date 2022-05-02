67e29776af56ce43f44666dfec500d2beed10784
michaelskyba <michaelskyba1411@gmail.com>
2022-05-02 13:53:21 -0400

    Implement win screen

--- games/2022/21/src/Game.svelte
+++ games/2022/21/src/Game.svelte
@@ -1,7 +1,9 @@
 <script>
 	export let players
 	let eliminated = []
+
 	let dead = false
+	let done = false
 
 	let turn = 0
 	let counter = 21
@@ -21,19 +23,18 @@
 			players = [...players.slice(0, turn), ...second]
 
 			dead = true
+
+			if (players.length == 1) done = true
+
+			// The turn is already progressed by the player leaving, so we need
+			// to cancel out the next turn++
+			turn--
 		}
 
 		turn++
 		if (turn >= players.length) turn = 0
 	}
 </script>
-
-<label for="players">In-game</label>
-<ol id="players">
-	{#each players as player}
-		<li>{player}</li>
-	{/each}
-</ol>
 
 {#if dead}
 	<label for="eliminated">Eliminated</label>
@@ -44,14 +45,25 @@
 	</ol>
 {/if}
 
-<p>Current turn: {players[turn]}</p>
+{#if !done}
+	<label for="players">In-game</label>
+	<ol id="players">
+		{#each players as player}
+			<li>{player}</li>
+		{/each}
+	</ol>
 
-<hr>
+	<h3>Current turn: {players[turn]}</h3>
 
-<h3>Counter: {counter}</h3>
-<input type="button" value="Take 1" on:click={() => take(1)} />
-<input type="button" value="Take 2" on:click={() => take(2)} />
-<input type="button" value="Take 3" on:click={() => take(3)} />
+	<hr>
+
+	<h3>Counter: {counter}</h3>
+	<input type="button" value="Take 1" on:click={() => take(1)} />
+	<input type="button" value="Take 2" on:click={() => take(2)} />
+	<input type="button" value="Take 3" on:click={() => take(3)} />
+{:else}
+	<h3>Winner: {players[0]}</h3>
+{/if}
 
 <style>
 	ol {
