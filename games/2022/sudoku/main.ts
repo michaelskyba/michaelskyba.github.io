9fe21592d7cb8c6f0c24ee764d6ae2b9fc4866d1
michaelskyba <michaelskyba1411@gmail.com>
2022-05-03 13:46:32 -0400

    sudoku: create HTML table for sudoku

--- games/2022/sudoku/main.ts
+++ games/2022/sudoku/main.ts
@@ -1,11 +1,23 @@
+const table = document.getElementById("grid")
+
 // Gets problems/1.txt to problems/50.txt
 let problemFile = `problems/${Math.round(Math.random() * 49) + 1}.txt`
-
 let r = new XMLHttpRequest()
 r.open("GET", problemFile, true)
 
 r.onload = function() {
-	console.log(this.responseText)
+	// Create sudoku table
+	for (const row of this.responseText.split("\n")) {
+		let tr = document.createElement("tr")
+
+		for (const cell of row) {
+			let td = document.createElement("td")
+			td.innerHTML = cell
+			tr.appendChild(td)
+		}
+
+		table.appendChild(tr)
+	}
 }
 
 r.send()
