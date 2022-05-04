47da6c1ffaacf563266ef85f5e52d5d245e2743f
michaelskyba <michaelskyba1411@gmail.com>
2022-05-04 10:01:11 -0400

    sudoku: construct 9x9 array for sudoku table

--- games/2022/sudoku/main.ts
+++ games/2022/sudoku/main.ts
@@ -1,23 +1,36 @@
 const table = document.getElementById("grid")
+let grid = []
 
 // Gets problems/1.txt to problems/50.txt
 let problemFile = `problems/${Math.round(Math.random() * 49) + 1}.txt`
 let r = new XMLHttpRequest()
 r.open("GET", problemFile, true)
 
+// Create sudoku table
 r.onload = function() {
-	// Create sudoku table
+	let rowIdx = 0
+
 	for (const row of this.responseText.split("\n")) {
+
+		// Empty "\n" at the end
+		if (rowIdx == 9) continue
+
 		let tr = document.createElement("tr")
+		grid.push([])
 
 		for (const cell of row) {
 			let td = document.createElement("td")
 			td.innerHTML = cell
 			tr.appendChild(td)
+
+			grid[rowIdx].push(cell)
 		}
 
 		table.appendChild(tr)
+		rowIdx++
 	}
+
+	console.log(grid)
 }
 
 r.send()
