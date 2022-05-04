26b296337f39b8a4f00a5a99e668c74566b0d00e
michaelskyba <michaelskyba1411@gmail.com>
2022-05-04 10:23:03 -0400

    sudoku: implement screen/grid updating

--- games/2022/sudoku/main.ts
+++ games/2022/sudoku/main.ts
@@ -1,6 +1,8 @@
 const table = document.getElementById("grid")
 let grid = []
 
+let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 // Gets problems/1.txt to problems/50.txt
 let problemFile = `problems/${Math.round(Math.random() * 49) + 1}.txt`
 let r = new XMLHttpRequest()
@@ -18,6 +20,8 @@ r.onload = function() {
 		let tr = document.createElement("tr")
 		grid.push([])
 
+		let colIdx = 0
+
 		for (const cell of row) {
 			let td = document.createElement("td")
 			td.innerHTML = cell
@@ -25,13 +29,26 @@ r.onload = function() {
 
 			if (cell == "0") {
 				td.className = "edit"
-				td.onclick = () => {
-					console.log(cell, row)
+
+				// Using e.g. "colIdx" instead of making these temp variables
+				// would set it to 9, the value after the loop is done.
+
+				let tempCol = colIdx
+				let tempRow = rowIdx
+
+				td.onclick = function() {
+					let newIdx = digits.indexOf(grid[tempRow][tempCol]) + 1
+					if (newIdx == 10) newIdx = 0
+
+					let newValue = digits[newIdx]
+					grid[tempRow][tempCol] = newValue
+					this.innerHTML = newValue
 				}
 			}
 			else td.className = "keep"
 
 			grid[rowIdx].push(cell)
+			colIdx++
 		}
 
 		table.appendChild(tr)
