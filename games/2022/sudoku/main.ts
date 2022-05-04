db2c384003ba199d732eb35b5bf59f04858815c5
michaelskyba <michaelskyba1411@gmail.com>
2022-05-04 13:13:19 -0400

    sudoku: better win screen

--- games/2022/sudoku/main.ts
+++ games/2022/sudoku/main.ts
@@ -137,5 +137,14 @@ submit.onclick = () => {
 		return
 	}
 
+	// Win
+
+	for (const tr of table.children) {
+		for (const td of tr.children) {
+			td.className = "edit"
+			td.onclick = null
+		}
+	}
+	submit.style.display = "none"
 	gameStatus.innerHTML = "All cells valid (win)"
 }
