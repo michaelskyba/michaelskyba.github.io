d1b07431fc7a7490570ce7711b56dfa242545df0
michaelskyba <michaelskyba1411@gmail.com>
2022-05-04 10:56:17 -0400

    sudoku: Add emptyu submit button

--- games/2022/sudoku/main.ts
+++ games/2022/sudoku/main.ts
@@ -1,4 +1,6 @@
 const table = document.getElementById("grid")
+const submit = document.getElementById("submit")
+
 let grid = []
 
 let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -64,8 +66,10 @@ r.onload = function() {
 		table.appendChild(tr)
 		rowIdx++
 	}
-
-	console.log(grid)
 }
 
 r.send()
+
+submit.onclick = () => {
+	console.log(grid)
+}
