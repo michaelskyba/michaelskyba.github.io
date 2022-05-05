995c5102156811c39cfe9bf0e85eed9dbe561d86
michaelskyba <michaelskyba1411@gmail.com>
2022-05-05 14:07:34 -0400

    Game.svelte: rematch button

--- games/2022/tic-tac-toe/src/Game.svelte
+++ games/2022/tic-tac-toe/src/Game.svelte
@@ -4,24 +4,32 @@
 	export let players
 	let turn = 0
 
-	let grid = [
-		[" ", " ", " "],
-		[" ", " ", " "],
-		[" ", " ", " "]
-	]
+	let grid
+	let win
 
-	let win = {
-		"exists": false,
-		"coords": [
-			[false, false, false],
-			[false, false, false],
-			[false, false, false]
+	const newMatch = () => {
+		grid = [
+			[" ", " ", " "],
+			[" ", " ", " "],
+			[" ", " ", " "]
 		]
+
+		win = {
+			"exists": false,
+			"coords": [
+				[false, false, false],
+				[false, false, false],
+				[false, false, false]
+			]
+		}
 	}
+	newMatch()
 </script>
 
 {#if win.exists}
 	<p>Winner: {players[turn == 1 ? 0 : 1]}</p>
+
+	<input type="button" value="Rematch" on:click={newMatch}>
 {:else}
 	<p>Current turn: {players[turn]}</p>
 {/if}
