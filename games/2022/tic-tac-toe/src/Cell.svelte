f92193144803dee19cdbf52aa37f05d035b81ec9
michaelskyba <michaelskyba1411@gmail.com>
2022-05-05 13:00:17 -0400

    ttt: draw basic grid

---
+++ games/2022/tic-tac-toe/src/Cell.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let ri
+	export let ci
+	export let grid
+</script>
+
+<td>{ri} {ci} {grid[ri][ci]}</td>
--- games/2022/tic-tac-toe/src/Game.svelte
+++ games/2022/tic-tac-toe/src/Game.svelte
@@ -1,5 +1,23 @@
 <script>
+	import Cell from "./Cell.svelte"
+
 	export let players
+
+	let grid = [
+		["E", "E", "E"],
+		["E", "E", "E"],
+		["E", "E", "E"]
+	]
 </script>
+
+<table>
+	{#each grid as row, ri}
+	<tr>
+		{#each row as cell, ci}
+		<Cell {ri} {ci} {grid} />
+		{/each}
+	</tr>
+	{/each}
+</table>
 
 <p>{players}</p>
