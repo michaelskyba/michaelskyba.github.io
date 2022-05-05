ce80188a5b3e2bb1002802f823970c27ce68e04c
michaelskyba <michaelskyba1411@gmail.com>
2022-05-05 12:39:27 -0400

    Setup.svelte: implement swap button

--- games/2022/tic-tac-toe/src/Setup.svelte
+++ games/2022/tic-tac-toe/src/Setup.svelte
@@ -1,4 +1,18 @@
 <script>
+	let player1 = "X"
+	let player2 = "O"
+
+	const swap = () => {
+		player1 += player2
+		player2 = player1[0]
+		player1 = player1[1]
+	}
 </script>
 
-<p>Setup</p>
+<ul>
+	<li>Player 1: {player1}</li>
+	<li>Player 2: {player2}</li>
+</ul>
+
+<input type="button" value="Swap" on:click={swap}>
+<input type="button" value="Confirm">
