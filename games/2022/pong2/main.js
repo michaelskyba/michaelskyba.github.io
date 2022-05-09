97a9d529f0016c441c20f6858d0fab206926e5e8
michaelskyba <michaelskyba1411@gmail.com>
2022-05-09 13:06:26 -0400

    add images to canvas

--- games/2022/pong2/main.js
+++ games/2022/pong2/main.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var background = document.getElementById("background");
+var ball = document.getElementById("ball");
+var paddle = document.getElementById("paddle");
+setInterval(function () {
+    ctx.drawImage(background, 0, 0);
+    ctx.drawImage(ball, 50, 50);
+    ctx.drawImage(paddle, 50, 150);
+}, 10);
--- games/2022/pong2/main.ts
+++ games/2022/pong2/main.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world!")
+const canvas = document.getElementById("canvas") as HTMLCanvasElement
+const ctx = canvas.getContext("2d")
+
+const background = document.getElementById("background") as HTMLImageElement
+const ball = document.getElementById("ball") as HTMLImageElement
+const paddle = document.getElementById("paddle") as HTMLImageElement
+
+setInterval(() => {
+	ctx.drawImage(background, 0, 0)
+	ctx.drawImage(ball, 50, 50)
+	ctx.drawImage(paddle, 50, 150)
+
+}, 10)
