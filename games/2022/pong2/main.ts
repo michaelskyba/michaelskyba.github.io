d691d272b8793dce72c2c3b1c24c6819d44e63eb
michaelskyba <michaelskyba1411@gmail.com>
2022-05-09 13:19:59 -0400

    pong2: use objects to group functions and values

--- games/2022/pong2/main.ts
+++ games/2022/pong2/main.ts
@@ -1,13 +1,34 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d")
 
-const background = document.getElementById("background") as HTMLImageElement
-const ball = document.getElementById("ball") as HTMLImageElement
-const paddle = document.getElementById("paddle") as HTMLImageElement
+const gameWidth = canvas.width
+const gameHeight = canvas.height
+
+let background = {
+	"img": document.getElementById("background") as HTMLImageElement,
+	draw: () => {ctx.drawImage(background.img, 0, 0)}
+}
+
+let ball = {
+	"x": canvas.width / 2 - 25,
+	"y": 10,
+	"img": document.getElementById("ball") as HTMLImageElement,
+	"speed": 1.10,
+
+	draw: () => {ctx.drawImage(ball.img, ball.x, ball.y)}
+}
+
+let paddle = {
+	"x": canvas.width / 2 - 50,
+	"y": 330,
+	"img": document.getElementById("paddle") as HTMLImageElement,
+
+	draw: () => {ctx.drawImage(paddle.img, paddle.x, paddle.y)}
+}
 
 setInterval(() => {
-	ctx.drawImage(background, 0, 0)
-	ctx.drawImage(ball, 50, 50)
-	ctx.drawImage(paddle, 50, 150)
+	background.draw()
 
+	ball.draw()
+	paddle.draw()
 }, 10)
