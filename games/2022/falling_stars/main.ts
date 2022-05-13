914438985e506d576bda7daaa599c05821faa221
michaelskyba <michaelskyba1411@gmail.com>
2022-05-13 09:11:28 -0400

    stars: movement

--- games/2022/falling_stars/main.ts
+++ games/2022/falling_stars/main.ts
@@ -1,6 +1,25 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d")
 
+let pressed = {
+	left: false,
+	right: false,
+	down: false,
+	up: false
+}
+document.onkeydown = e => {
+	if (e.code == "ArrowRight") pressed.right = true
+	if (e.code == "ArrowLeft") pressed.left = true
+	if (e.code == "ArrowUp") pressed.up = true
+	if (e.code == "ArrowDown") pressed.down = true
+}
+document.onkeyup = e => {
+	if (e.code == "ArrowRight") pressed.right = false
+	if (e.code == "ArrowLeft") pressed.left = false
+	if (e.code == "ArrowUp") pressed.up = false
+	if (e.code == "ArrowDown") pressed.down = false
+}
+
 class Drawing {
 	img: HTMLImageElement
 	x: number
@@ -18,18 +37,34 @@ class Drawing {
 	}
 }
 
+class Player extends Drawing {
+	constructor(id: string, x: number, y: number) {
+		super(id, x, y)
+	}
+
+	move() {
+		if (pressed.left) this.x -= 3
+		if (pressed.right) this.x += 3
+		if (pressed.up) this.y -= 3
+		if (pressed.down) this.y += 3
+	}
+}
+
 const background = new Drawing("background", 0, 0)
-const player = new Drawing("player", 50, 50)
+const player = new Player("player", 50, 50)
 const star = new Drawing("star", 200, 200)
 const enemy = new Drawing("enemy", 300, 300)
 
 class Game {
 	draw() {
+		player.move()
+
 		background.draw()
 		player.draw()
 		star.draw()
 		enemy.draw()
 	}
+
 }
 
 let game = new Game()
