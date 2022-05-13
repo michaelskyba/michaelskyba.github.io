ee506182a19938bbb20de7da3de620362e40c761
michaelskyba <michaelskyba1411@gmail.com>
2022-05-13 08:50:23 -0400

    stars: primitive drawing loop

--- games/2022/falling_stars/main.ts
+++ games/2022/falling_stars/main.ts
@@ -1,1 +1,28 @@
-console.log("Hello, world!")
+const canvas = document.getElementById("canvas") as HTMLCanvasElement
+const ctx = canvas.getContext("2d")
+
+class Img {
+	img: HTMLImageElement
+
+	constructor(id: string) {
+		this.img = document.getElementById(id) as HTMLImageElement
+	}
+}
+
+const backgroundImg = new Img("background")
+const playerImg = new Img("player")
+const starImg = new Img("star")
+const enemyImg = new Img("enemy")
+
+class Game {
+	draw() {
+		ctx.drawImage(backgroundImg.img, 0, 0)
+		ctx.drawImage(playerImg.img, 50, 50)
+		ctx.drawImage(starImg.img, 200, 200)
+		ctx.drawImage(enemyImg.img, 300, 300)
+	}
+}
+
+let game = new Game()
+
+setInterval(game.draw, 1)
