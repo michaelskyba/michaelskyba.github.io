3792e2cb21fc965457acab6ae33e3c19821f5081
michaelskyba <michaelskyba1411@gmail.com>
2022-05-15 23:13:03 -0400

    robots: basic drawing

--- games/2022/robots/main.ts
+++ games/2022/robots/main.ts
@@ -1,1 +1,22 @@
-console.log("Hello, world!")
+const canvas = document.getElementById("canvas") as HTMLCanvasElement
+const ctx = canvas.getContext("2d")
+
+const background = document.getElementById("background") as HTMLImageElement
+const robot_blue1 = document.getElementById("robot_blue1") as HTMLImageElement
+const robot_blue2 = document.getElementById("robot_blue2") as HTMLImageElement
+const robot_green1 = document.getElementById("robot_green1") as HTMLImageElement
+const robot_green2 = document.getElementById("robot_green2") as HTMLImageElement
+const robot_red1 = document.getElementById("robot_red1") as HTMLImageElement
+const robot_red2 = document.getElementById("robot_red2") as HTMLImageElement
+const robot_yellow1 = document.getElementById("robot_yellow1") as HTMLImageElement
+const robot_yellow2 = document.getElementById("robot_yellow2") as HTMLImageElement
+
+ctx.drawImage(background, 0, 0)
+ctx.drawImage(robot_blue1, 0, 0)
+ctx.drawImage(robot_blue2, 0, 0)
+ctx.drawImage(robot_green1, 0, 0)
+ctx.drawImage(robot_green2, 0, 0)
+ctx.drawImage(robot_red1, 0, 0)
+ctx.drawImage(robot_red2, 0, 0)
+ctx.drawImage(robot_yellow1, 0, 0)
+ctx.drawImage(robot_yellow2, 0, 0)
