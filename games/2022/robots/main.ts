e5ff80d84a1cd4dbd06aaa41c05897c5d10f2a96
michaelskyba <michaelskyba1411@gmail.com>
2022-05-16 12:17:50 -0400

    robots: implement robot animation

--- games/2022/robots/main.ts
+++ games/2022/robots/main.ts
@@ -1,10 +1,21 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d")
 
+const RNG = (min, max) => {
+	return Math.round(Math.random() * (max - min)) + min
+}
+
 class Robot {
 	img: HTMLImageElement[]
 
+	switchThreshold: number
+	lastSwitch: number
+	costume = 0
+
 	constructor(id) {
+		this.switchThreshold = RNG(350, 1000)
+		this.lastSwitch = new Date().getTime()
+
 		this.img = [
 			document.getElementById(id + "1") as HTMLImageElement,
 			document.getElementById(id + "2") as HTMLImageElement
@@ -12,7 +23,12 @@ class Robot {
 	}
 
 	draw() {
-		ctx.drawImage(this.img[0], 0, 0)
+		if (new Date().getTime() - this.lastSwitch >= this.switchThreshold) {
+			this.costume = 1 - this.costume
+			this.lastSwitch = new Date().getTime()
+		}
+
+		ctx.drawImage(this.img[this.costume], 0, 0)
 	}
 }
 
