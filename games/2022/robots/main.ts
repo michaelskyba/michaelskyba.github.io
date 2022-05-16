c5e7210fe684df661c001cd661c6f8d2f40e8b33
michaelskyba <michaelskyba1411@gmail.com>
2022-05-16 12:20:46 -0400

    robots: create all four robots

--- games/2022/robots/main.ts
+++ games/2022/robots/main.ts
@@ -12,17 +12,18 @@ class Robot {
 	lastSwitch: number
 	costume = 0
 
-	constructor(id) {
+	constructor(colour: string) {
 		this.switchThreshold = RNG(350, 1000)
 		this.lastSwitch = new Date().getTime()
 
 		this.img = [
-			document.getElementById(id + "1") as HTMLImageElement,
-			document.getElementById(id + "2") as HTMLImageElement
+			document.getElementById(`robot_${colour}1`) as HTMLImageElement,
+			document.getElementById(`robot_${colour}2`) as HTMLImageElement
 		]
 	}
 
 	draw() {
+		// Switch robot animation frame every (switchThreshold) ms
 		if (new Date().getTime() - this.lastSwitch >= this.switchThreshold) {
 			this.costume = 1 - this.costume
 			this.lastSwitch = new Date().getTime()
@@ -32,7 +33,11 @@ class Robot {
 	}
 }
 
-const robot_blue = new Robot("robot_blue")
+let robots = []
+const colours = ["red", "green", "blue", "yellow"]
+for (const colour of colours) {
+	robots.push(new Robot(colour))
+}
 
 const background = {
 	img: document.getElementById("background") as HTMLImageElement,
@@ -43,7 +48,10 @@ const background = {
 
 function step() {
 	background.draw()
-	robot_blue.draw()
+
+	for (const robot of robots) {
+		robot.draw()
+	}
 
 	window.requestAnimationFrame(step)
 }
