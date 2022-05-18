95036b42510f56abf8165cfd693cfbd15b1ab446
michaelskyba <michaelskyba1411@gmail.com>
2022-05-18 09:10:17 -0400

    savo: basic mainMenu object

--- games/2022/savo/main.ts
+++ games/2022/savo/main.ts
@@ -1,5 +1,22 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d")
 
-ctx.fillStyle = "#2c8898"
-ctx.fillRect(0, 0, 1325, 725)
+const mainMenu = {
+	x: 0,
+
+	draw() {
+		ctx.fillStyle = "#2c8898"
+		ctx.fillRect(0, 0, 1325, 725)
+
+		ctx.font = "48px monospace"
+		ctx.fillStyle = "#000000"
+		ctx.fillText("Malfacile Gajnita Savo", this.x, 50)
+
+		this.x++
+
+		window.requestAnimationFrame(this.draw)
+	}
+}
+mainMenu.draw = mainMenu.draw.bind(mainMenu)
+
+window.requestAnimationFrame(mainMenu.draw)
