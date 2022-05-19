e32347df0eabb46a62dbbed4eb26ca504720b04a
michaelskyba <michaelskyba1411@gmail.com>
2022-05-19 10:37:57 -0400

    use textBox for speech

--- games/2022/savo/src/claudiaHouse.ts
+++ games/2022/savo/src/claudiaHouse.ts
@@ -2,15 +2,24 @@ import c from "./canvas"
 import player from "./player"
 
 import dialogue from "./dialogue_1"
+import textBox from "./textBox"
 
 const claudiaHouse = {
 	scene: {
 		dialogue: dialogue.main,
 		playing: true,
-		frame: 0
+		frame: 0,
+
+		// Let TypeScript infer the types from here and then set them properly
+		// in init(). In here, "this" throws a "Object possibly undefined" error
+		speaker: new textBox("", 0, 0, 0, "serif", "", ""),
+		speech: new textBox("", 0, 0, 0, "serif", "", "")
 	},
 
 	init() {
+		let line = this.scene.dialogue[this.scene.frame]
+		this.scene.speech = new textBox(line[1], 50, 600, 30, "serif", "white", "black")
+
 		document.onkeydown = e => {
 			player.handleKey("keydown", e)
 		}
@@ -26,13 +35,18 @@ const claudiaHouse = {
 		c.fillStyle = "#982c61"
 		c.frect(400, 0, 925, 725)
 
+		/*
 		let line = this.scene.dialogue[this.scene.frame]
 		let speaker = line[0]
 		let speech = line[1]
 
+		if (line[0]) this.scene.speech.draw()
+
 		c.fillStyle = "#2c8898"
-		if (speaker) c.text(speaker, 20, 20)
 		c.text(speech, 20, 100)
+		*/
+
+		this.scene.speech.draw()
 
 		// .move() doesn't really belong in draw() but it's fine for now
 		player.move()
