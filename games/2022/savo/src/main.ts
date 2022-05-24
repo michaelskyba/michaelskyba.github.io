3d09d0fe559a59acca8b3a0cc2805bee1dfe198a
michaelskyba <michaelskyba1411@gmail.com>
2022-05-24 13:25:38 -0400

    faster game start for development

--- games/2022/savo/src/main.ts
+++ games/2022/savo/src/main.ts
@@ -1,4 +1,19 @@
+/*
 import mainMenu from "./menus/mainMenu"
 
 document.onkeydown = e => mainMenu.handleInput(e.code)
 window.requestAnimationFrame(mainMenu.draw)
+*/
+
+// (Faster start for testing)
+
+import perinthus from "./overworld/perinthus"
+import claudiaHouse from "./fixed/claudiaHouse"
+import player from "./play/player"
+
+claudiaHouse.init()
+
+player.x = 0
+player.y = 0
+
+window.requestAnimationFrame(perinthus.draw)
