3330c2af94f452b1d186d25fa0290387c3a7a3f1
michaelskyba <michaelskyba1411@gmail.com>
2022-05-24 13:39:43 -0400

    add doors to houses (later interactable)

--- games/2022/savo/src/overworld/perinthus.ts
+++ games/2022/savo/src/overworld/perinthus.ts
@@ -3,13 +3,19 @@ import player from "../play/player"
 
 import Block from "./Block"
 
-const blocks = [
-	new Block(-425, -150, 400, 300, "yellow")
+const buildings = [
+	new Block(-425, -150, 400, 300, "yellow"),
+	new Block(50, -1150, 300, 250, "lightblue")
 ]
 
-const decorations = [
+const roads = [
 	new Block(-50, -50, 155, 100, "gray"),
 	new Block(100, -1000, 200, 2000, "gray")
+]
+
+const doors = [
+	new Block(-50, -50, 25, 100, "brown"),
+	new Block(150, -925, 100, 25, "brown")
 ]
 
 const perinthus = {
@@ -19,12 +25,16 @@ const perinthus = {
 
 		player.move([])
 
-		for (const block of decorations) {
-			block.draw(player.x, player.y)
+		for (const road of roads) {
+			road.draw(player.x, player.y)
 		}
 
-		for (const block of blocks) {
-			block.draw(player.x, player.y)
+		for (const building of buildings) {
+			building.draw(player.x, player.y)
+		}
+
+		for (const door of doors) {
+			door.draw(player.x, player.y)
 		}
 
 		player.draw("overworld")
