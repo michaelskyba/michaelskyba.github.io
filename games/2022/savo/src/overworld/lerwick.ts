69a48b3cd2a4da28a9608f57b3f48745b7dde734
michaelskyba <michaelskyba1411@gmail.com>
2022-06-07 10:24:13 -0400

    slightly more sophisticated town layout

--- games/2022/savo/src/overworld/lerwick.ts
+++ games/2022/savo/src/overworld/lerwick.ts
@@ -4,8 +4,25 @@ import player from "../game/player"
 import Block from "./Block"
 
 const buildings = [
+	// Akvedukto
+	new Block(-330, -100, 300, 200, "lightblue"),
+
 	// Nero's house
-	new Block(500, -50, 400, 300, "maroon")
+	new Block(700, -1300, 400, 300, "maroon"),
+
+	// Tiberius's house
+	new Block(1850, -650, 300, 400, "#c3272b")
+]
+
+const roads = [
+	// 1. Connecting to Akvedukto and 2
+	new Block(-50, -50, 1000, 100, "gray"),
+
+	// 2. Connecting to Nero, 1, and 3
+	new Block(850, -1000, 100, 1000, "gray"),
+
+	// 3. Conencting to 2 and Tiberius
+	new Block(850, -500, 1000, 100, "gray")
 ]
 
 const lerwick = {
@@ -14,15 +31,25 @@ const lerwick = {
 		document.onkeyup = event => player.handleKey("keyup", event.code)
 	},
 
+	transitions(): string | null {
+		return null
+	},
+
 	move: () => player.move("overworld", buildings),
 
 	draw() {
 		c.fillStyle = "#ff8936"
 		c.frect(0, 0, 1325, 725)
 
+		for (const road of roads) {
+			road.draw(player.x, player.y)
+		}
+
 		for (const building of buildings) {
 			building.draw(player.x, player.y)
 		}
+
+		c.text(`(${player.x}, ${player.y})`, 50, 50)
 
 		player.draw("overworld")
 	}
