30296e196af004091a2f20b009030f87700b1865
michaelskyba <michaelskyba1411@gmail.com>
2022-06-09 18:16:02 -0400

    neroHouse wall vaues

--- games/2022/savo/src/fixed/neroHouse.ts
+++ games/2022/savo/src/fixed/neroHouse.ts
@@ -4,18 +4,87 @@ import Wall from "./Wall"
 
 let wallColour = "maroon"
 let walls = [
-	new Wall(0, 0, 1325, 25, wallColour),
-	new Wall(0, 0, 25, 1325, wallColour),
+	// First room
+	[
+		new Wall(0, 0, 1325, 25, wallColour),
+		new Wall(0, 0, 25, 1325, wallColour),
 
-	// Bottom with intersection
-	new Wall(0, 700, 512.5, 25, wallColour),
-	new Wall(812.5, 700, 612.5, 25, wallColour),
+		// Bottom intersection
+		new Wall(0, 700, 512.5, 25, wallColour),
+		new Wall(812.5, 700, 612.5, 25, wallColour),
 
-	// Initially solid right wall
-	new Wall(1300, 0, 25, 725, wallColour)
+		// Right intersection
+		new Wall(1300, 0, 25, 212.5, wallColour),
+		new Wall(1300, 512.5, 25, 212.5, wallColour)
+	],
+
+	// Second room - entered through the right of first room
+	[
+		new Wall(0, 700, 1325, 25, wallColour),
+
+		// Left intersection
+		new Wall(0, 0, 25, 212.5, wallColour),
+		new Wall(0, 512.5, 25, 212.5, wallColour),
+
+		// Top intersection
+		new Wall(0, 0, 512.5, 25, wallColour),
+		new Wall(812.5, 0, 612.5, 25, wallColour),
+
+		new Wall(1300, 0, 25, 725, wallColour)
+	],
+
+	// Third room - entered through the top of second room
+	[
+		new Wall(0, 0, 1325, 25, wallColour),
+
+		// Left intersection
+		new Wall(0, 0, 25, 212.5, wallColour),
+		new Wall(0, 512.5, 25, 212.5, wallColour),
+
+		// Bottom intersection
+		new Wall(0, 700, 512.5, 25, wallColour),
+		new Wall(812.5, 700, 612.5, 25, wallColour),
+
+		new Wall(1300, 0, 25, 725, wallColour)
+	],
+
+	// Fourth room - entered through the left of third room
+	[
+		new Wall(0, 0, 25, 1325, wallColour),
+		new Wall(0, 0, 1325, 25, wallColour),
+
+		// Right intersection
+		new Wall(1300, 0, 25, 212.5, wallColour),
+		new Wall(1300, 512.5, 25, 212.5, wallColour),
+
+		// Bottom intersection
+		new Wall(0, 700, 512.5, 25, wallColour),
+		new Wall(812.5, 700, 612.5, 25, wallColour),
+	],
+
+	// Fifth (Nero's) room - entered through the bottom of the fourth room
+	[
+		new Wall(0, 0, 25, 725, wallColour),
+		new Wall(0, 700, 1325, 25, wallColour),
+		new Wall(1300, 0, 25, 725, wallColour),
+
+		// Top intersection
+		new Wall(0, 0, 512.5, 25, wallColour),
+		new Wall(812.5, 0, 612.5, 25, wallColour),
+	],
+
+	// (Nero's) room - locked
+	[
+		new Wall(0, 0, 1325, 25, wallColour),
+		new Wall(0, 0, 25, 1325, wallColour),
+		new Wall(1300, 0, 25, 725, wallColour),
+		new Wall(0, 700, 1325, 25, wallColour)
+	]
 ]
 
 const neroHouse = {
+	room: 0,
+
 	init() {
 		document.onkeydown = event => player.handleKey("keydown", event.code)
 		document.onkeyup = event => player.handleKey("keyup", event.code)
@@ -28,7 +97,7 @@ const neroHouse = {
 
 		player.draw("fixed")
 
-		for (const wall of walls) {
+		for (const wall of walls[this.room]) {
 			wall.draw()
 		}
 	}
