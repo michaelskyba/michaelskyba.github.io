6214067048e85386c1f594038302f17cebdcc9f3
michaelskyba <michaelskyba1411@gmail.com>
2022-06-15 13:05:31 -0400

    clear zoom out

--- games/2022/savo/src/game/main.ts
+++ games/2022/savo/src/game/main.ts
@@ -1,3 +1,7 @@
+/*
+Name: Michael Skyba, Malfacile Gajnita Savo
+*/
+
 // Official start
 import mainMenu from "../menus/mainMenu"
 import steps from "./steps"
@@ -9,43 +13,3 @@ document.getElementById("load").onclick = () => {
 	// Hide load button
 	document.getElementById("load").style.display = "none"
 }
-
-// Quick main menu
-/*
-import mainMenu from "../menus/mainMenu"
-import steps from "./steps"
-mainMenu.init()
-window.requestAnimationFrame(steps.mainMenu)
-document.getElementById("load").style.display = "none"
-*/
-
-// Lerwick
-/*
-import lerwick from "../overworld/lerwick"
-import steps from "./steps"
-import player from "./player"
-
-lerwick.init()
-
-player.x = 0
-player.y = 0
-
-window.requestAnimationFrame(steps.lerwick)
-document.getElementById("load").style.display = "none"
-*/
-
-// Nero's house/room
-/*
-import neroHouse from "../fixed/neroHouse"
-import steps from "./steps"
-import player from "./player"
-
-neroHouse.init()
-neroHouse.neroRoomInit()
-
-player.x = 637.5
-player.y = 50
-
-window.requestAnimationFrame(steps.neroHouse)
-document.getElementById("load").style.display = "none"
-*/
