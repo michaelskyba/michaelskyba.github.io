6c3ffdc3d89dcc5eb79cc2bdc9465711e6e7bae4
michaelskyba <michaelskyba1411@gmail.com>
2022-06-26 17:03:01 -0400

    augustRoom idea

---
+++ games/2022/savo/src/events/8.ts
@@ -0,0 +1,71 @@
+// Dialogue in Augustus' room
+
+/*
+Wait, didn't you die in AD 14?
+Either you're supposed to be dead, or I'm not supposed to be alive.
+
+It's the former. You're right; I'm supposed to be dead.
+And yet, I am not.
+Does that fact intensely trouble you?
+
+It does. How can you be content with contradiction?
+Have you never heard of the explosion principle?
+
+I have, but it does not apply. Your view of 'logic' is too narrow-minded.
+Regardless, I can already infer why you are here.
+
+What's your conclusion?
+
+You're here to suffer, are you not?
+You're capable of slaying Nero flawlessly and yearn for defeat?
+
+What if I'm not?
+
+If you can't do something as simple as overpowering poor Nero...
+You're out of your territory here.
+
+What if I am?
+
+You'll still be disappointed. Go home, or you'll regret it.
+
+Thanks for the offer, but I think I'll stay.
+You'll probably even fail to provide a sufficient amount of suffering...
+By the way, was that Tiberius Caesar Augustus in the other room?
+
+No, obviously. Don't get confused just because his name tag uses his first name.
+
+But wouldn't 'Britannicus' be more accurate, then?
+
+Who cares?
+
+...
+
+Here is my proposal.
+You're clearly not thinking straight, so I'll give you three minutes.
+If, after the three minutes, your intent for misfortune exists, I won't stop myself.
+
+You think that will make me change my mind?
+Although, waiting three minutes does seem kind of annoying...
+
+Of course it does. So, leave.
+
+I wonder if it's possible to skip this...
+
+(New state)
+	Stickied:
+	Hmm... I wish I had Tiberius's time machine right now...
+
+	(movement enabled)
+	(display timer)
+	(keep playing climactic_return)
+	(have the exit open)
+
+(once the timer is over)
+	Three minutes is up. My intent is everlasting.
+
+	I see. Let's begin, then.
+
+	(Start climax_reasoning)
+	(once the drum drops, add more enemies)
+		think of a good system
+*/
---
+++ games/2022/savo/src/fixed/augustusRoom.ts
@@ -0,0 +1,7 @@
+class Room {
+	init() {
+	}
+}
+
+const room = new Room()
+export default room
