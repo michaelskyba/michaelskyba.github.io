85819db1fc51de770be556b8f5819f59c5cd9016
michaelskyba <michaelskyba1411@gmail.com>
2022-07-07 12:27:35 -0400

    Augustus dialogue formatting

--- games/2022/savo/src/events/8.ts
+++ games/2022/savo/src/events/8.ts
@@ -1,71 +1,51 @@
 // Dialogue in Augustus' room
 
-/*
-Wait, didn't you die in AD 14?
-Either you're supposed to be dead, or I'm not supposed to be alive.
+const dialogue = [
+	// Initial enter
+	[
+		["Claudia", "..."],
+		["Claudia", "...?"],
+		["Claudia", "Wait, didn't you die in AD 14?"],
+		["Claudia", "Either you're supposed to be dead, or I'm not supposed to be alive."],
+		["Augustus", "It's the former. You're right; I'm supposed to be dead."],
+		["Augustus", "And yet, I am not."],
+		["Augustus", "Does that fact intensely trouble you?"],
+		["Claudia", "It does. How can you be content with contradiction?"],
+		["Claudia", "Have you never heard of the explosion principle?"],
+		["Augustus", "I have, but it does not apply. Your view of 'logic' is too narrow-minded."],
 
-It's the former. You're right; I'm supposed to be dead.
-And yet, I am not.
-Does that fact intensely trouble you?
+		["Augustus", "Regardless, I can already infer why you are here."],
+		["Claudia", "What's your conclusion?"],
+		["Augustus", "You're here to suffer, are you not?"],
+		["Augustus", "You're capable of slaying Nero flawlessly and yearn for defeat?"],
+		["Claudia", "What if I'm not?"],
+		["Augustus", "If you can't do something as simple as overpowering poor Nero..."],
+		["Augustus", "You're out of your territory here."],
+		["Claudia", "What if I am?"],
+		["Augustus", "You'll still be disappointed. Go home, or you'll regret it."],
+		["Claudia", "Thanks for the offer, but I think I'll stay."],
+		["Claudia", "You'll probably even fail to provide a sufficient amount of suffering..."],
 
-It does. How can you be content with contradiction?
-Have you never heard of the explosion principle?
+		["Claudia", "By the way, was that Tiberius Caesar Augustus in the other room?"],
+		["Augustus", "No, obviously. Don't get confused just because his name tag uses his first name."],
+		["Claudia", "But wouldn't 'Britannicus' be more accurate, then?"],
+		["Augustus", "Who cares?"],
+		["Claudia", "..."],
 
-I have, but it does not apply. Your view of 'logic' is too narrow-minded.
-Regardless, I can already infer why you are here.
+		["Augustus", "Here is my proposal."],
+		["Augustus", "You're clearly not thinking straight, so I'll give you three minutes."],
+		["Augustus", "If, after the three minutes, your intent for misfortune exists, I won't stop myself."],
+		["Claudia", "You think that will make me change my mind?"],
+		["Claudia", "Although, waiting three minutes does seem kind of annoying..."],
+		["Augustus", "Of course it does. So, leave."],
+		["Claudia", "I wonder if it's possible to 𝘀𝗸𝗶𝗽 𝘁𝗵𝗶𝘀..."]
+	],
 
-What's your conclusion?
+	// After the three-minute wait
+	[
+		["Claudia", "Three minutes is up. My intent is everlasting."],
+		["Augustus", "I see. Let's begin, then."]
+	]
+]
 
-You're here to suffer, are you not?
-You're capable of slaying Nero flawlessly and yearn for defeat?
-
-What if I'm not?
-
-If you can't do something as simple as overpowering poor Nero...
-You're out of your territory here.
-
-What if I am?
-
-You'll still be disappointed. Go home, or you'll regret it.
-
-Thanks for the offer, but I think I'll stay.
-You'll probably even fail to provide a sufficient amount of suffering...
-By the way, was that Tiberius Caesar Augustus in the other room?
-
-No, obviously. Don't get confused just because his name tag uses his first name.
-
-But wouldn't 'Britannicus' be more accurate, then?
-
-Who cares?
-
-...
-
-Here is my proposal.
-You're clearly not thinking straight, so I'll give you three minutes.
-If, after the three minutes, your intent for misfortune exists, I won't stop myself.
-
-You think that will make me change my mind?
-Although, waiting three minutes does seem kind of annoying...
-
-Of course it does. So, leave.
-
-I wonder if it's possible to skip this...
-
-(New state)
-	Stickied:
-	Hmm... I wish I had Tiberius's time machine right now...
-
-	(movement enabled)
-	(display timer)
-	(keep playing climactic_return)
-	(have the exit open)
-
-(once the timer is over)
-	Three minutes is up. My intent is everlasting.
-
-	I see. Let's begin, then.
-
-	(Start climax_reasoning)
-	(once the drum drops, add more enemies)
-		think of a good system
-*/
+export default dialogue
